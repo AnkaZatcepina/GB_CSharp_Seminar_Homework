8a90fe1b90f792fe01c10c3810932d813fb1323d
Anna Zatsepina <anka.zatcepina@gmail.com>
2022-09-20 15:11:46 +0300

    Add Lesson 6

---
+++ Lesson6_CountPositiveNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Ввод элементов массива с клавиатуры
+double[] CreateArrayNumbers(int size)
+{
+    var array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число {i}: ");
+        array[i] = double.Parse(Console.ReadLine());;
+    }
+    return array;
+}
+
+//Подсчёт положительных
+int CountPositiveNumbersInArray(double[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+
+//Ввод массива
+double[] array = CreateArrayNumbers(6);
+//Подсчёт положительных
+int countPositiveNumbers = CountPositiveNumbersInArray(array);
+//Вывод результата
+Console.WriteLine($"Количество положительных чисел: {countPositiveNumbers}");
---
+++ Lesson6_GetIntersectionPoint/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Поиск точки пересечения
+double[] GetIntersectionPoint(int k1, int b1, int k2, int b2)
+{
+    var point = new double[2];
+    //Вычисляем X по формуле
+    point[0] = Convert.ToDouble(b2 - b1) / Convert.ToDouble(k1 - k2);
+    //Вычисляем Y по формуле
+    point[1] = k1 * point[0] + b1;
+
+    return point;
+}
+
+//Ввод параметров
+Console.WriteLine("Введите коэффициенты для прямых y = k1 * x + b1 и y = k2 * x + b2");
+Console.Write("Введите коэффициент k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите коэффициент b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите коэффициент k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите коэффициент b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+
+//Поиск точки пересечения
+var point = GetIntersectionPoint(k1, b1, k2, b2);
+
+//Вывод результата
+Console.WriteLine($"Точка пересечения двух прямых: ({point[0]};{point[1]})");
